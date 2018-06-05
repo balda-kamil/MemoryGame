79cc1368dfe9c1353a859463882bbaaa86e3567c
balda-kamil <baldakamil@gmail.com>
2018-06-05 07:35:01 +0200

    remove picked color from array mechanism added

--- game.js
+++ game.js
@@ -1,11 +1,15 @@
 const cardColors = ["red", "red", "green", "green", "blue", "blue", "brown", "brown", "yellow", "yellow", "gray", "gray", "cadetblue", "cadetblue", "violet", "violet", "lightgreen", "lightgreen"];
 
 let cards = document.querySelectorAll("div");
-
+cards = [...cards];
 
 const init = function(){
-    console.log(Math.ceil(Math.random))
-}
+    cards.forEach((card) => {
+        const position = Math.floor(Math.random() * cardColors.length);
+        card.classList.add(cardColors[position])
+        cardColors.splice(position, 1)
+    })
 
+}
 
 init()
